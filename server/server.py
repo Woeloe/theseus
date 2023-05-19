1677f09dbc08e91e1bdd1f0390e90855361a42a6
Senne Goeminne <Senne.Goeminne@ugent.be>
2023-05-19 15:18:43 +0200

    levels en level/nr request toegevoegd

--- server/server.py
+++ server/server.py
@@ -1,3 +1,7 @@
+import os
+import json
+from pathlib import Path
+
 from flask import Flask, request, abort
 from flask_cors import CORS
 
@@ -10,11 +14,34 @@ CORS(app)
 
 @app.route('/levels')
 def fetch_levels():
-    raise NotImplementedError
+    if request.method == 'GET':
+        count = 0
+        for path in os.listdir("levels"):
+
+            if os.path.isfile(os.path.join("levels", path)):
+                count += 1
+        print(count)
+        data = {
+            "aantal_levels": count
+        }
+        return data
 
 @app.route('/level/<int:level>')
 def fetch_level(level: int):
-    raise NotImplementedError
+    print(level)
+    if request.method == 'GET':
+        path = Path('levels/level' + str(level) + '.json')
+        print(path)
+
+        if path.is_file():
+            with open(path) as f:
+                data = json.load(f)
+        else:
+            data = {
+                "foutboodschap": "Puzzel " + str(level) + " bestaat niet."
+            }
+        return data
+
 
 @app.route('/random_level')
 def fetch_random_level():
@@ -29,4 +56,4 @@ def update_highscore(level: int):
     raise NotImplementedError
 
 if __name__ == '__main__':
-    app.run(port=3000)
+    app.run(port=3000, debug=True)
